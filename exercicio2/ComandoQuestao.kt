c2c03bc85eacb21ef9666aacba805ee21747bef0
kimalextst <kimtorresst@gmail.com>
2022-05-11 13:21:48 -0300

    .

--- exercicio2/ComandoQuestao.kt
+++ exercicio2/ComandoQuestao.kt
@@ -1,2 +1,27 @@
 package exercicio2
 
+/*Implemente uma classe abstracta de nome Forma onde são
+declarados dois métodos abstractos:
+- calcularArea(): Float
+- calcularPerimetro(): Float
+
+Crie, como subclasse de Forma, uma classe de nome Rectangulo cujas
+instâncias são caracterizadas pelos atributos lado e altura ambos do tipo
+Float. Implemente na classe Rectangulo os métodos herdados de Forma e
+outros que ache necessários.
+
+Crie, como subclasse de Forma, uma classe de nome Circulo cujas
+instâncias são caracterizadas pelo atributo raio do tipo Float. Implemente
+na classe Circulo os métodos herdados de Forma e outros que ache
+necessários.
+
+Crie, como subclasse de Rectangulo, uma classe de nome Quadrado cujas
+instâncias são caracterizadas por terem os atributos lado e altura com o
+mesmo valor.
+
+Elabore um programa de teste onde é declarado um array, de dimensão 5..
+Nesse array devem ser guardadas instâncias de Rectangulo, Circulo e
+Quadrado seguindo uma ordem aleatória.
+Depois implemente um ciclo que percorra o array invocando,
+relativamente a cada um dos objectos guardados, os métodos calcularArea
+e calcularPerimetro*/
